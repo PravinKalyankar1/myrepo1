f1741831d9cf7364775d5b9f6dd84d2affe78678
PravinKalyankar1 <kalyankarpravin97@gmail.com>
2026-01-15 19:05:25 +0530

    Update opertors.py

--- opertors.py
+++ opertors.py
@@ -1,8 +1,20 @@
 # Operators
 # Arithmatic operators
 num1=40
-num2=20
+num2=3
 print(num1-num2)  # Substration
 print (num1 + num2) # Addtion
 print (num1 * num2) # Multiplication
 print (num1/num2) # Dividation
+print (num1 % num2) # Modulas
+print (num1**num2) # Exponenet(power)
+print (num1 // num2) # Floor divison
+
+Output:
+37
+43
+120
+13.333333333333334
+1
+64000
+13
